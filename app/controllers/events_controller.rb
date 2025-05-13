5153261b434615cc38ce55bbdcdb20ac67810708
Thisaga <thisagajay@gmail.com>
2025-05-13 15:58:45 +0530

    feat: implement update and destroy methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,11 +25,50 @@ class EventsController < ApplicationController
         end
     end
 
-    def index
+    def index # GET EVENTS
         event = Event.all
         render json: event
     end
 
+    def update # PUT / PATCH
+        event = Event.find_by(uuid: params[:id])
+
+        if event.nil?
+            render json: { error: "Event not found" }, status: :not_found
+            return
+        end
+
+        if event.notification_sent # prevent setting to true if notification already true
+            render json: { error: "Notification already sent" }, status: :bad_request
+            return
+        end
+
+        if event.update(notification_sent: true)
+            render json: event, status: :ok
+        else
+            render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy # DELETE
+        event = Event.find_by(uuid: params[:id])
+
+        if event.nil?
+            render json: { error: "Event not found" }, status: :not_found
+            return
+        end
+
+        if event.is_deleted
+            render json: { error: "Event already deleted" }, status: :bad_request
+            return
+        end
+
+        if event.update(is_deleted: true)
+            head :no_content
+        else
+            render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
     private
     def event_params
         params.require(:event).permit(
